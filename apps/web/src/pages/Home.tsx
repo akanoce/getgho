c418ccd58e9f90000dd33df357974d9797b8a09e
Erik Nucibella <eriknucib@live.it>
2024-01-12 22:00:14 +0100

    feat: add connected chain and disconnect button

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import { ConnectKitButton } from "connectkit";
-import { useAccount } from "wagmi";
+import { useAccount, useDisconnect, useNetwork } from "wagmi";
 
 export const Home = () => {
   const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { chain } = useNetwork();
 
   if (!isConnected) return <ConnectKitButton />;
 
   return (
     <div>
       <h1>Home</h1>
+      <p>Connected network: {chain?.name}</p>
       <p>Address: {address}</p>
+      <button onClick={() => disconnect()}>Disconnect</button>
     </div>
   );
 };
