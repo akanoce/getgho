9a8d1b2ed37bbb6e47be661d87960917996316b2
Piero Bassa <pierobassa222@gmail.com>
2024-01-13 19:26:42 +0100

    feat: sample home page for demo purposes

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,19 +1,59 @@
-import { ConnectKitButton } from "connectkit";
-import { useAccount, useDisconnect, useNetwork } from "wagmi";
+import { ShimmerButton } from '../components';
+import { useTurnkey } from '../hooks';
 
 export const Home = () => {
-  const { address, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { chain } = useNetwork();
+    const { wallet, login, createSubOrgAndWallet } = useTurnkey();
 
-  if (!isConnected) return <ConnectKitButton />;
+    console.log(wallet);
 
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Connected network: {chain?.name}</p>
-      <p>Address: {address}</p>
-      <button onClick={() => disconnect()}>Disconnect</button>
-    </div>
-  );
+    return (
+        <div className="flex justify-center items-center h-[100vh]">
+            <a>{JSON.stringify(wallet) ?? ''}</a>
+            <div className="flex flex-col gap-y-4">
+                {!wallet && (
+                    <>
+                        <ShimmerButton
+                            className="h-14 shadow-2xl"
+                            shimmerColor="purple"
+                            shimmerSize="0.1em"
+                            background="white"
+                            onClick={createSubOrgAndWallet}
+                        >
+                            <span className="px-1 text-center font-bold leading-none">
+                                Get a wallet
+                            </span>
+                        </ShimmerButton>
+                        <ShimmerButton
+                            className="h-14 shadow-2xl"
+                            shimmerColor="purple"
+                            shimmerSize="0.1em"
+                            background="white"
+                            onClick={login}
+                        >
+                            <span className="px-1 text-center font-bold leading-none">
+                                Have a wallet?
+                            </span>
+                        </ShimmerButton>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+
+    // ConnectKit should be used when depositing instead of logging in
+    /* 
+    const { address, isConnected } = useAccount();
+    const { disconnect } = useDisconnect();
+    const { chain } = useNetwork();
+
+    if (!isConnected) return <ConnectKitButton />;
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Connected network: {chain?.name}</p>
+            <p>Address: {address}</p>
+            <button onClick={() => disconnect()}>Disconnect</button>
+        </div>
+    ); */
 };
