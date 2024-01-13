8d3309a331391d5be38fbf66632fee6c6b861d88
Piero Bassa <pierobassa222@gmail.com>
2024-01-13 19:27:10 +0100

    chore: minor linting

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,14 +1,14 @@
-import { WagmiConfig } from "wagmi";
-import { ConnectKitProvider } from "connectkit";
-import { Home } from "./pages/Home";
-import { config } from "./config";
+import { WagmiConfig } from 'wagmi';
+import { ConnectKitProvider } from 'connectkit';
+import { Home } from './pages/Home';
+import { config } from './config';
 
 export const App = () => {
-  return (
-    <WagmiConfig config={config}>
-      <ConnectKitProvider>
-        <Home />
-      </ConnectKitProvider>
-    </WagmiConfig>
-  );
+    return (
+        <WagmiConfig config={config}>
+            <ConnectKitProvider>
+                <Home />
+            </ConnectKitProvider>
+        </WagmiConfig>
+    );
 };
