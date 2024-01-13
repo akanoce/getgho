c449751b2c135d07b1fa9b9eb0c236a4c8abb3d7
grenos <vasilis.green@gmail.com>
2024-01-13 17:24:08 +0100

    feat: add passkeys package WIP

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,19 +1,27 @@
 import { ConnectKitButton } from "connectkit";
 import { useAccount, useDisconnect, useNetwork } from "wagmi";
+// import { createTurnkeySigner } from "passkeys";
+import { useCallback } from "react";
 
 export const Home = () => {
-  const { address, isConnected } = useAccount();
-  const { disconnect } = useDisconnect();
-  const { chain } = useNetwork();
+    const { address, isConnected } = useAccount();
+    const { disconnect } = useDisconnect();
+    const { chain } = useNetwork();
 
-  if (!isConnected) return <ConnectKitButton />;
+    const turnKeySinger = useCallback(async () => {
+        // const signer = await createTurnkeySigner();
+        // console.log({ signer });
+    }, []);
 
-  return (
-    <div>
-      <h1>Home</h1>
-      <p>Connected network: {chain?.name}</p>
-      <p>Address: {address}</p>
-      <button onClick={() => disconnect()}>Disconnect</button>
-    </div>
-  );
+    if (!isConnected) return <ConnectKitButton />;
+
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Connected network: {chain?.name}</p>
+            <p>Address: {address}</p>
+            <button onClick={() => disconnect()}>Disconnect</button>
+            <button onClick={turnKeySinger}>Turnkey</button>
+        </div>
+    );
 };
