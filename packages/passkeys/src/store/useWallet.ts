4c759ecce81abb05e18d6bba6efbd9f69946be62
Davide Carpini <carpini.davide@gmail.com>
2024-01-14 18:12:18 +0100

    fix: import

--- packages/passkeys/src/store/useWallet.ts
+++ packages/passkeys/src/store/useWallet.ts
@@ -1,18 +1,18 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
-import { TWalletDetails } from "../../../apps/web/src/model";
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { TWalletDetails } from '../model';
 
 interface WalletState {
-  wallet: TWalletDetails | undefined;
-  setWallet: (wallet: TWalletDetails | undefined) => void;
+    wallet: TWalletDetails | undefined;
+    setWallet: (wallet: TWalletDetails | undefined) => void;
 }
 
 export const useWallet = create<WalletState>()(
-  persist(
-    (set) => ({
-      wallet: undefined,
-      setWallet: (wallet: TWalletDetails | undefined) => set({ wallet }),
-    }),
-    { name: "lfgho/wallet" }
-  )
+    persist(
+        (set) => ({
+            wallet: undefined,
+            setWallet: (wallet: TWalletDetails | undefined) => set({ wallet })
+        }),
+        { name: 'lfgho/wallet' }
+    )
 );
