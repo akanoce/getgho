b844a2daf0cedd2752c928555361fef41dafe24c
Erik Nucibella <eriknucib@live.it>
2024-01-14 17:25:53 +0100

    chore: bump prettier

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-    root: true,
-    env: { browser: true, es2020: true },
-    extends: ['@repo/eslint-config', 'plugin:react-hooks/recommended'],
-    plugins: ['react-refresh'],
-    rules: {
-        'react-refresh/only-export-components': [
-            'warn',
-            { allowConstantExport: true }
-        ]
-    }
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: ["@repo/eslint-config", "plugin:react-hooks/recommended"],
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
 };
