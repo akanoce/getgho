64621839e6d9ff6f8ed156aaae12e384508bc234
Erik Nucibella <eriknucib@live.it>
2024-01-14 17:14:48 +0100

    chore: reuse eslint config

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,21 +1,12 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
-    'prettier'
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-    'prettier/prettier': ["error", { "singleQuote": true, "trailingComma": "none", "tabWidth": 4 }],
-  },
-}
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['@repo/eslint-config', 'plugin:react-hooks/recommended'],
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true }
+        ]
+    }
+};
