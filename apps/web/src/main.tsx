f7d58b9089ccfac40e9e4c69621a79e48efa5032
grenos <vasilis.green@gmail.com>
2024-01-17 00:58:47 +0100

    refactor: init sdk wip

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,6 +6,7 @@ import { App } from './App';
 import { AaveContractsProvider } from './providers';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ConnectKitProvider } from 'connectkit';
+import { LfghoContextProvider } from '@repo/lfgho-sdk';
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -23,15 +24,17 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <WagmiConfig config={config}>
-            <QueryClientProvider client={queryClient}>
-                <AaveContractsProvider>
-                    <ConnectKitProvider>
-                        <App />
-                    </ConnectKitProvider>
-                </AaveContractsProvider>
-                {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-            </QueryClientProvider>
-        </WagmiConfig>
+        <LfghoContextProvider>
+            <WagmiConfig config={config}>
+                <QueryClientProvider client={queryClient}>
+                    <AaveContractsProvider>
+                        <ConnectKitProvider>
+                            <App />
+                        </ConnectKitProvider>
+                    </AaveContractsProvider>
+                    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+                </QueryClientProvider>
+            </WagmiConfig>
+        </LfghoContextProvider>
     </React.StrictMode>
 );
