15039ab06bfd9122da5ce50a49078c69d6469f4a
Erik Nucibella <eriknucib@live.it>
2024-01-16 22:57:04 +0100

    feat(ReservesIncentives): ui to render aave's formatted reserves with incentives

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Deposit, ShimmerButton } from '@/components';
 import { Address } from 'viem';
 import { UserSummary } from '@/components/UserSummary';
+import { ReservesIncentives } from '@/components/ReservesIncentives';
 
 type Props = {
     wallet: Address;
@@ -12,6 +13,7 @@ export const Home = ({ wallet, logout }: Props) => {
         <div className="flex justify-center items-center h-[100vh]">
             {wallet && (
                 <div className="flex flex-col gap-y-4">
+                    <ReservesIncentives />
                     <UserSummary address={wallet} />
                     <a>Wallet Address : {JSON.stringify(wallet) ?? ''}</a>
 
