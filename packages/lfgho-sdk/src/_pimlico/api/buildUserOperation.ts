2b1dc35d1eaff4b2ff1b32ddb38ae7bfa5f07216
Piero Bassa <pierobassa222@gmail.com>
2024-01-18 23:06:17 +0100

    feat: send tx with sponsorship from pimlico paymaster

--- packages/lfgho-sdk/src/_pimlico/api/buildUserOperation.ts
+++ packages/lfgho-sdk/src/_pimlico/api/buildUserOperation.ts
@@ -23,14 +23,6 @@ export const buildUserOperation = async ({
     const gasPriceResult = await bundlerClient.getUserOperationGasPrice();
     const nonce = await getAccountNonce(publicClient, { entryPoint, sender });
 
-    if (nonce !== 0n) {
-        console.log(
-            'Deployment UserOperation previously submitted, skipping...'
-        );
-
-        return;
-    }
-
     const userOperation: Partial<UserOperation> = {
         sender,
         nonce,
@@ -40,7 +32,7 @@ export const buildUserOperation = async ({
         maxFeePerGas: gasPriceResult.fast.maxFeePerGas,
         maxPriorityFeePerGas: gasPriceResult.fast.maxPriorityFeePerGas,
         signature:
-            '0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c',
+            '0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c', // Dummy signature
         ...rest
     };
 
