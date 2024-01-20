d89f3f3a75830776865d3cabf8879cfeb627caac
Davide Carpini <carpini.davide@gmail.com>
2024-01-20 18:54:33 +0100

    fix

--- apps/web/src/pages/Home.tsx
+++ apps/web/src/pages/Home.tsx
@@ -1,8 +1,16 @@
 import { Address } from 'viem';
-import { FormControl, FormLabel, Switch, VStack } from '@chakra-ui/react';
+import {
+    Button,
+    FormControl,
+    FormLabel,
+    Switch,
+    VStack
+} from '@chakra-ui/react';
 import { UserAssets } from '@/components/UserAssets';
 import { useSponsoredTxFlag } from '@repo/lfgho-sdk';
 import { ChangeEvent, useCallback } from 'react';
+import { Funnel } from '@/components/Funnel';
+import { useAccountAdapter } from '@/hooks/useAccountAdapter';
 
 type Props = {
     wallet: Address;
@@ -10,6 +18,7 @@ type Props = {
 
 export const Home = ({ wallet }: Props) => {
     const { setIsSPonsored } = useSponsoredTxFlag();
+    const { logout } = useAccountAdapter();
 
     const handleOnChange = useCallback(
         (event: ChangeEvent<HTMLInputElement>) => {
@@ -26,13 +35,13 @@ export const Home = ({ wallet }: Props) => {
                 </FormLabel>
                 <Switch id="sposnored" size="lg" onChange={handleOnChange} />
             </FormControl>
-
             <UserAssets address={wallet} />
+            <Funnel address={wallet} />
             {/* <ReservesIncentives address={wallet} />
             <UserSummary address={wallet} />
             <Deposit />
             <SendTx />
-            <SendErc20Tx />
+            <SendErc20Tx /> */}
             <Button
                 variant={'solid'}
                 colorScheme="purple"
@@ -40,7 +49,7 @@ export const Home = ({ wallet }: Props) => {
                 size="lg"
             >
                 Logout
-            </Button> */}
+            </Button>
         </VStack>
     );
 };
