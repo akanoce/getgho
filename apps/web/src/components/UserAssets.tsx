1c383c91870b0d275d8d8909280b865f50632ec0
Erik Nucibella <eriknucib@live.it>
2024-01-20 17:12:12 +0100

    fix: render available underlying

--- apps/web/src/components/UserAssets.tsx
+++ apps/web/src/components/UserAssets.tsx
@@ -1,17 +1,31 @@
-import { useUserReservesIncentives } from '@/api';
+import { useReserves, useUserReservesIncentives } from '@/api';
 import { useAccountAdapter } from '@/hooks/useAccountAdapter';
-import { Box, Button, Heading, Spinner, VStack } from '@chakra-ui/react';
+import {
+    Box,
+    Button,
+    HStack,
+    Heading,
+    Spinner,
+    VStack
+} from '@chakra-ui/react';
 
 type Props = {
     address: string;
 };
 export const UserAssets = ({ address }: Props) => {
+    const { logout } = useAccountAdapter();
+
     const { data: userReserves, isLoading: userReservesLoading } =
         useUserReservesIncentives(address);
 
-    const { logout } = useAccountAdapter();
+    const { data: reserves, isLoading: reservesLoading } = useReserves();
 
-    console.log({ userReserves });
+    console.log({ userReserves, reserves });
+
+    const availableUnderlying =
+        userReserves?.formattedUserSummary.userReservesData.filter(
+            (reserve) => reserve.underlyingBalance !== '0'
+        );
     return (
         <VStack spacing={4} alignItems={'stretch'} w="full">
             <Button
@@ -27,9 +41,14 @@ export const UserAssets = ({ address }: Props) => {
                 {userReservesLoading ? (
                     <Spinner />
                 ) : (
-                    <Heading as="h2" size="lg">
-                        User Assets
-                    </Heading>
+                    <VStack w="full" spacing={4}>
+                        {availableUnderlying?.map((reserve) => (
+                            <HStack w="full" key={reserve.underlyingAsset}>
+                                <Box>{reserve.reserve.symbol}</Box>
+                                <Box>{reserve.underlyingBalanceUSD} USD</Box>
+                            </HStack>
+                        ))}
+                    </VStack>
                 )}
             </Box>
         </VStack>
